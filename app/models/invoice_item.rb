2be472fb87a6614698f88c57226f36e2c34512eb
Leena <bleenaweb238@gmail.com>
2023-03-01 22:00:59 +0530

    Updated item-total gross amd date

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,18 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :tax
   belongs_to :invoice
+
+  before_save :calculate_total
+  after_save :checksum
+
+  def calculate_total
+    tax = Tax.find(self.tax_id).percentage
+    self.item_total = (self.units * self.unit_cost) + tax
+  end
+
+  def checksum
+    tax = Tax.find(self.tax_id).percentage
+    invoice = Invoice.find self.invoice_id
+    invoice.update(total_gross: self.item_total, total_taxes: tax, total_net: self.item_total, invoice_date: DateTime.now)
+  end
 end
