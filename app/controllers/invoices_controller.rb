3c18db5b4c424ad702a7c497e775f12e6ec7695f
Leena <bleenaweb238@gmail.com>
2023-03-02 18:25:01 +0530

    Added check for login on every page

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class InvoicesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_invoice, only: %i[show edit update destroy]
 
   def index
--- app/controllers/taxes_controller.rb
+++ app/controllers/taxes_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TaxesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_tax, only: %i[show edit update destroy]
 
   def index
