ab0d5ddf79212a1330415970265e720c2d9fd765
Leena <bleenaweb238@gmail.com>
2023-03-03 00:04:45 +0530

    Added invoice layout

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items, dependent: :destroy
+  validates :invoice_date, presence: true
 
   accepts_nested_attributes_for :invoice_items, reject_if: :all_blank, allow_destroy: true
 end
