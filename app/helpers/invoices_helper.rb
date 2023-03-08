a700394e2459f53fe39cd551f0643048eec0d3a5
Leena <bleenaweb238@gmail.com>
2023-03-08 12:33:48 +0530

    stimulus updates test

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -2,4 +2,15 @@
 
 # InvoicesHelper
 module InvoicesHelper
+  def select_options_for(hash)
+    options = ''
+    hash.each do |key, value|
+      options += option_tag(key, value)
+    end
+    options.html_safe
+  end
+
+  def option_tag(label, value)
+    "<option value=#{value}>#{label}</option>"
+  end
 end
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,56 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+
+  static targets = [
+    "units", "unitCost", "tax",
+    "totalGross", "totalNet", "totalTax",
+    "itemTotalWithTax", "taxAmount", "itemTotal"
+  ]
+
+  calculate(e) {
+    e.preventDefault();
+    debugger
+    if (this.unitCostTarget.value != 0.0) {
+      this.itemTotalTarget.value = this.unitsTarget.value * this.unitCostTarget.value
+      this.taxAmountTarget.value = (this.itemTotalTarget.value / 100) * parseFloat(this.taxTarget.value)
+      this.itemTotalWithTaxTarget.value = parseFloat(this.itemTotalTarget.value) + parseFloat(this.taxAmountTarget.value)
+      let itemTotals = 0.0
+      let itemTotalWithTaxs = 0.0
+      let taxAmounts = 0.0
+      for(let itt of this.itemTotalTargets) {
+        itemTotals += parseFloat(itt.value)
+      }
+      for(let itt of this.taxAmountTargets) {
+        taxAmounts += parseFloat(itt.value)
+      }
+      for(let itt of this.itemTotalWithTaxTargets) {
+        itemTotalWithTaxs += parseFloat(itt.value)
+      }
+      debugger
+      this.totalNetTarget.value = itemTotals
+      this.totalGrossTarget.value = itemTotalWithTaxs
+      this.totalTaxTarget.value = taxAmounts.toFixed(2)
+    }
+  }
+
+  recalculate(e) {
+    e.preventDefault();
+    this.unitCostTarget.parentNode.parentNode.remove()
+    let itemTotals = 0.0
+    let itemTotalWithTaxs = 0.0
+    let taxAmounts = 0.0
+    for(let itt of this.itemTotalTargets) {
+      itemTotals += parseFloat(itt.value)
+    }
+    for(let itt of this.taxAmountTargets) {
+      taxAmounts += parseFloat(itt.value)
+    }
+    for(let itt of this.itemTotalWithTaxTargets) {
+      itemTotalWithTaxs += parseFloat(itt.value)
+    }
+    this.totalNetTarget.value = itemTotals
+    this.totalGrossTarget.value = itemTotalWithTaxs
+    this.totalTaxTarget.value = taxAmounts.toFixed(2)
   }
 }
